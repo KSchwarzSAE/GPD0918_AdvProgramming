7c746ae212b193683802d6319c3ab0f0087242d7
Kevin Schwarz <k.schwarz@sae.edu>
2019-11-12 14:38:13 +0100

    Unterricht 12.11.19

---
+++ Unity/Assets/Scenes/LoadWorldPart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadWorldPart : MonoBehaviour
+{
+
+    public string m_sceneToLoad;
+
+    private bool m_loaded;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_loaded) return;
+
+        m_loaded = true;
+
+        StartCoroutine(LoadScene());
+     }
+
+    private IEnumerator LoadScene()
+    {
+        AsyncOperation loadScene
+            = SceneManager.LoadSceneAsync(m_sceneToLoad, LoadSceneMode.Additive);
+        loadScene.allowSceneActivation = true;
+
+        yield return loadScene;
+        Debug.Log("Scene was loaded");
+    }
+
+}
